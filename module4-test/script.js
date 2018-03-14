3ff5378d785b928b5808d43270a433ba1b9828e4
annbabuji <33451536+annbabuji@users.noreply.github.com>
2018-03-14 14:06:00 +0300

    Update script.js

--- module4-test/script.js
+++ module4-test/script.js
@@ -2,7 +2,6 @@
 
 var names = ["Yaakov", "John","Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-
 for (var i = 0; i < names.length; i++) {
 
   var firstLetter = names[i].charAt(0).toLowerCase();
